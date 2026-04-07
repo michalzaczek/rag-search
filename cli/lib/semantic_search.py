99918fb7bfb359a84a7ab08f476c801e722038c5
Michal <michal.zaczek@interia.pl>
2026-04-07 13:17:07 +0200

    feat: add generate embedding method to ss class

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,15 +1,27 @@
 from sentence_transformers import SentenceTransformer
 
 
-class SemanticSearch:
-    def __init__(self) -> None:
-        # Load the model
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
 def verify_model():
     ss = SemanticSearch()
     print(f"Model loaded: {ss.model}")
     print(f"Max sequence length: {ss.model.max_seq_length}")
 
-    # self.model.encode(text)
+
+def embed_text(text):
+    ss = SemanticSearch()
+    embedding = ss.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
+
+
+class SemanticSearch:
+    def __init__(self) -> None:
+        # Load the model
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    def generate_embedding(self, text):
+        if text.trim() == "":
+            raise ValueError("Error: Text cannot be empty")
+
+        return self.model.encode(text)
