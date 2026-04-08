af2b47d0e2a1495f8e45e855be96d768d0b40926
Michal <michal.zaczek@interia.pl>
2026-04-08 15:05:33 +0200

    feat: add semantic search command to cli

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,8 @@
 
 from cli.lib import semantic_search
 from cli.parsers.semantic_search_parsers import args, parser
-from cli.lib.semantic_search import verify_model
+from cli.lib.semantic_search import SemanticSearch, verify_model
+from core.utils import load_json_file
 
 
 def main():
@@ -20,6 +21,15 @@ def main():
         case "embedquery":
             semantic_search.embed_query_text(args.query)
 
+        case "search":
+            ss = SemanticSearch()
+            documents = load_json_file("data/movies.json")["movies"]
+            ss.load_or_create_embeddings(documents)
+            results = ss.search(args.query, args.limit)
+            for idx, movie in enumerate(results, start=1):
+                print(
+                    f'{idx}. {movie["title"]} (score: {movie["score"]:.4f})\n{movie["description"]}\n'
+                )
         case _:
             parser.print_help()
 
